b7f0b4008e96fcd6797560c7725dd70f16968e2c
vctrchappot <victor.chappot@digitecgalaxus.ch>
2026-08-31 11:09:52 +0200

    04 Exception Handling

---
+++ 04-exception-handling/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _04_exception_handling;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = SumInteger.RetrieveNumbers();
+        var result = SumInteger.SumNumbers(input);
+    }
+}
+
---
+++ 04-exception-handling/SumInteger.cs
@@ -0,0 +1,32 @@
+﻿namespace _04_exception_handling;
+
+public class SumInteger
+{
+    public static int[] RetrieveNumbers()
+    {
+        var numbers = new int[2];
+        
+        Console.WriteLine("(Your first number is processed with the second)");
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number:");
+                var input = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                numbers[i] = input;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid integer!");
+            }
+        }
+
+        return numbers;
+    }
+
+    public static int SumNumbers(int[] numbers)
+    {
+        var result = numbers[0] + numbers[1];
+        return result;
+    }
+}
