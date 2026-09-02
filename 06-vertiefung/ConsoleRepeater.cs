2a076ca69430dbd3be0a4526db31885d40f432f5
vctrchappot <victor.chappot@digitecgalaxus.ch>
2026-09-02 11:54:35 +0200

    Update ConsoleRepeater.cs

--- 06-vertiefung/ConsoleRepeater.cs
+++ 06-vertiefung/ConsoleRepeater.cs
@@ -6,35 +6,35 @@ public class ConsoleRepeater
     {
         const char x = 'x';
         
-        Console.SetCursorPosition(0, 0);
-        Console.Write(x);
-        
         var widthPos = 0;
-        var topPos = 0;
+        var heightPos = 0;
+        
+        Console.SetCursorPosition(widthPos, heightPos);
+        Console.Write(x);
         
         while (true)
         {
             var pressedKey = Console.ReadKey(true);
 
-            if (MovementValidator.IsValidMovement(widthPos, topPos, pressedKey))
+            if (MovementValidator.IsValidMovement(widthPos, heightPos, pressedKey))
             {
                 switch (pressedKey.Key)
                 {
                     case ConsoleKey.W:
-                        topPos--;
+                        heightPos--;
                         break;
                     case ConsoleKey.A:
                         widthPos--;
                         break;
                     case ConsoleKey.S:
-                        topPos++;
+                        heightPos++;
                         break;
                     case ConsoleKey.D:
                         widthPos++;
                         break;
                 }
                 Console.Clear();
-                Console.SetCursorPosition(widthPos, topPos);
+                Console.SetCursorPosition(widthPos, heightPos);
                 Console.Write(x);
             }
         }
